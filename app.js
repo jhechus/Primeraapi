5726886aa3119b4fbf261716ebc2156d6017e6fd
jhechus <jess.osm1412@gmail.com>
2022-04-29 20:04:17 -0500

    se agrega los get, put y delete para mi primer proyecto de api, para poder iniciarlo se necesita de la aplicacio postman

--- app.js
+++ app.js
@@ -17,8 +17,35 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: 'Jesus'}
+    res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(201).json({message: 'Created'})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(200).json({message: 'Updated'})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorer DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body //parametros del cliente
+    res.status(200).json({message: 'Deleted'})
+})
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
+
